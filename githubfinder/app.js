7246d266b960ff280c229d8804655a7f59f9328d
danidz96 <37420305+danidz96@users.noreply.github.com>
2018-07-27 21:14:44 +0200

    add github profile information

---
+++ githubfinder/app.js
@@ -0,0 +1,34 @@
+// Init github
+const github = new Github;
+
+// Init UI
+const ui = new Ui;
+
+// Search input
+const searchUser = document.getElementById('searchUser');
+
+// Search input event listener
+searchUser.addEventListener('keypress' , e => {
+    // Get key
+    const key = e.which || e.keyCode;
+
+    // Get input text
+    const userText = e.target.value;
+
+    if (key === 13 && userText != '') {
+        // Make http call
+        github.getUser(userText)
+            .then(data => {
+                if (data.profile.message === 'Not Found') {
+                    // Show alert
+                    ui.clearProfile();
+                    ui.showAlert('User not found', 'alert alert-danger');
+                } else {
+                    // Show profile
+                    ui.showProfile(data.profile);
+                }
+            }).catch(err => {
+                console.log(err);
+            });
+    }
+});
---
+++ githubfinder/ui.js
@@ -0,0 +1,82 @@
+class Ui {
+    constructor() {
+        this.profile = document.getElementById('profile');
+    }
+
+    // Display profile UI
+    showProfile(user) {
+        let output = '';
+
+        output += `
+                    <div class="card card-body mb-3">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <img src="${user.avatar_url}" class="img-fluid rounded mb-2">
+                                <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-3">View Profile</a>
+                            </div>
+                            <div class="col-md-9">
+                                <span class="badge badge-primary">Public repos: ${user.public_repos}</span>
+                                <span class="badge badge-secondary">Public gists: ${user.public_gists}</span>
+                                <span class="badge badge-success">Followers: ${user.followers}</span>
+                                <span class="badge badge-info">Following: ${user.following}</span>
+                                <br><br>
+                                <ul class="list-group">
+                                    <li class="list-group-item">Company: ${user.company}</li>
+                                    <li class="list-group-item">Website/Blog: ${user.blog}</li>
+                                    <li class="list-group-item">Location: ${user.location}</li>
+                                    <li class="list-group-item">Member since:: ${user.created_at}</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <h3 class="page-heading mb-3">Lastest Repos</h3>
+                    <div class="repos"></div>    
+        `;
+
+        this.profile.innerHTML = output;
+    }
+
+    // Clear profile
+    clearProfile() {
+        this.profile.innerHTML = '';
+    }
+
+    // Show alert message
+    showAlert(message, className) {
+
+        // Clear alert
+        this.clearAlert();
+
+        // Create div
+        const div = document.createElement('div');
+
+        // Add classes
+        div.className = className;
+
+        // Add text
+        div.appendChild(document.createTextNode(message));
+
+        // Get parent
+        const container = document.querySelector('.searchContainer');
+
+        // Get search box
+        const search = document.querySelector('.search');
+
+        // Insert alert
+        container.insertBefore(div, search);
+
+        // Timeout after 3 seconds
+        setTimeout(() => {
+            this.clearAlert();
+        }, 3000);
+    }
+
+    // Clear alert message
+    clearAlert() {
+        const currentAlert = document.querySelector('.alert');
+
+        if (currentAlert) {
+            currentAlert.remove();
+        }
+    }
+}
